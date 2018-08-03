a843d947272606cbc650ea8982953ae98548a77d
Bwukki <bwukki@gmail.com>
2018-08-03 12:48:13 -0700

    Changed window text back to WillFromAFar

--- AcapellaDownloader/Form1.Designer.cs
+++ AcapellaDownloader/Form1.Designer.cs
@@ -95,7 +95,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Curtis Michels Ver 2.0 WillFromAfar";
+            this.Text = "WillFromAFar";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();

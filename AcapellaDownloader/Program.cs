6c6e4d904735153cd5b673610cd069b4eaa6e5f3
Weespin <wnmerukini@gmail.com>
2020-11-07 22:08:52 +0200

    I hate a typo below

--- AcapellaDownloader/Program.cs
+++ AcapellaDownloader/Program.cs
@@ -13,6 +13,7 @@ namespace AcapellaDownloader
 		        CLI.PassCLI(commandLineArgs);
 				return;
 	        }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
